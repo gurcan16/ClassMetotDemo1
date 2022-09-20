a2352735c95c4a7e0d6429f92e22f88438dafe23
gurcan16 <108872652+gurcan16@users.noreply.github.com>
2022-09-20 23:04:01 +0300

    Add project files.

---
+++ ClassMetotDemo1/CustomerManager.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    class CustomerManager
+    {
+        List<Customer> customersList = new List<Customer>();
+        public void Add(Customer customer)
+        {
+            Console.WriteLine("Müşteri Eklendi");
+            Console.WriteLine("Hoş Geldiniz " + customer.Name + " " + customer.Surname);
+            customersList.Add(customer);
+        }
+
+        public void Delete(Customer customer)
+        {
+            Console.WriteLine("Müşteri Silindi");
+            Console.WriteLine(customer.Name + " " + customer.Surname);
+        }
+
+
+        public void Show()
+        {
+            foreach (var item in customersList)
+            {
+                var customerInfo = $" {item.Id} {item.Name} {item.Surname}";
+                Console.WriteLine("Müşteri:" + customerInfo);
+            }
+
+            Console.WriteLine("Toplam Kullanıcı Sayısı:" + (customersList.Count));
+        }
+
+
+    }
+}
+
+
+
+
+
---
+++ ClassMetotDemo1/Program.cs
@@ -0,0 +1,37 @@
+﻿using ClassMetotDemo;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        Customer customer1 = new Customer();
+        customer1.Id = 27;
+        customer1.Name = "Gürcan";
+        customer1.Surname = "ALTIPARMAK";
+
+        Customer customer2 = new Customer();
+        customer2.Id = 16;
+        customer2.Name = "Barış";
+        customer2.Surname = "YARAŞ";
+
+        Customer[] customers = new Customer[] { customer1, customer2 };
+
+        foreach (var customer in customers)
+        {
+            Console.WriteLine(customer.Name + " " + customer.Surname);
+        }
+        Console.WriteLine(" ");
+
+        CustomerManager customerManager = new CustomerManager();
+
+        customerManager.Add(customer1);
+        Console.WriteLine(" ");
+        customerManager.Add(customer2);
+        Console.WriteLine("----------------------------");
+        customerManager.Delete(customer1);
+        Console.WriteLine(" ");
+        customerManager.Delete(customer2);
+        customerManager.Show();
+    }
+}
